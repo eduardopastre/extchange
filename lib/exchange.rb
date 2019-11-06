8a755d047e2c6cc34d109d9f8ac7436d5b78e41e
eduardo pastre <dupastre@gmail.com>
2019-11-06 00:18:59 -0200

    test to deploy

--- lib/exchange.rb
+++ lib/exchange.rb
@@ -4,12 +4,12 @@ require 'json'
 module Currency
   def self.exchange currency_base, currency_destination, quantity
     begin
-      #res = RestClient.get "http://api.fixer.io/latest?base=#{currency}&symbols=#{currency_destination}"
+      #res = RestClient.get"http://api.fixer.io/latest?base=#{currency}&symbols=#{currency_destination}"
       res = RestClient.get "https://api.exchangeratesapi.io/latest?base=#{currency_base}"
       value = JSON.parse(res.body)['rates'][currency_destination]
       return (value * quantity).round(2)
     rescue
-      return "A conversão falhou."
+      return "A conversão falhou"
     end
   end
 end
