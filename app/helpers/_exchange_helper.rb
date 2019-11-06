a5c5ebf3448ea2b39702daf3b4bc07bd1711bebc
eduardo pastre <dupastre@gmail.com>
2019-11-05 22:53:39 -0200

    changed foreing exchange api

--- app/helpers/_exchange_helper.rb
+++ app/helpers/_exchange_helper.rb
@@ -1,9 +1,38 @@
 module ExchangeHelper
   def currency_select
-    ['AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
-      'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
-      'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
-      'THB', 'TRY', 'USD', 'ZAR'
+    [
+      ['BRL - Real', 'BRL'],
+      ['USD - Dólar americano', 'USD'], 
+      ['EUR - Euro', 'EUR'],
+      ['AUD - Dólar australiano', 'AUD'], 
+      ['BGN - Lev búlgaro', 'BGN'], 
+      ['CAD - Dólar canadense', 'CAD'], 
+      ['CHF - Franco suíço', 'CHF'], 
+      ['CNY - Renminbi', 'CNY'], 
+      ['CZK - Coroa checa', 'CZK'], 
+      ['DKK - Coroa dinamarquesa', 'DKK'], 
+      ['GBP - Libra Esterlina', 'GBP'],
+      ['HKD - Dólar de Hong Kong', 'HKD'], 
+      ['HRK - Kuna croata', 'HRK'], 
+      ['HUF - Forint', 'HUF'], 
+      ['IDR - Rupia indonésia', 'IDR'], 
+      ['ILS - Shekel', 'ILS'], 
+      ['INR - Rupia indiana', 'INR'], 
+      ['JPY - Iene', 'JPY'], 
+      ['KRW - Won sul coreano', 'KRW'], 
+      ['MXN - Peso mexicano', 'MXN'],
+      ['MYR - Ringgit', 'MYR'], 
+      ['NOK - Coroa norueguesa', 'NOK'], 
+      ['NZD - Dólar da Nova Zelândia', 'NZD'], 
+      ['PHP - Peso filipino', 'PHP'], 
+      ['PLN - Zloty', 'PLN'], 
+      ['RON - Leu romeno', 'RON'], 
+      ['RUB - Rublo', 'RUB'], 
+      ['SEK - Coroa Sueca', 'SEK'], 
+      ['SGD - Dólar de Singapura', 'SGD'],
+      ['THB - Baht', 'THB'], 
+      ['TRY - Nova Lira turca', 'TRY'], 
+      ['ZAR - Rand', 'ZAR']
     ]
   end
 end
--- lib/exchange.rb
+++ lib/exchange.rb
@@ -1,14 +1,15 @@
 require 'rest-client'
 require 'json'
- 
+
 module Currency
-  def self.exchange currency, currency_destination, quantity
+  def self.exchange currency_base, currency_destination, quantity
     begin
-      res = RestClient.get "http://api.fixer.io/latest?base=#{currency}&symbols=#{currency_destination}"
+      #res = RestClient.get "http://api.fixer.io/latest?base=#{currency}&symbols=#{currency_destination}"
+      res = RestClient.get "https://api.exchangeratesapi.io/latest?base=#{currency_base}"
       value = JSON.parse(res.body)['rates'][currency_destination]
       return (value * quantity).round(2)
     rescue
-      return 'Problem in Conversion'
+      return "A conversão falhou"
     end
   end
 end
